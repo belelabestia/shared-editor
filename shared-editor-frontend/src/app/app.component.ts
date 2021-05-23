7411281e7c89986b836dc30ee3434e3ba3d20321
Marco Bellezza <bellezza.m@capp.it>
2021-05-23 14:56:16 +0200

    Avanzamenti FE
    Iniziata architettura HubConnection -> Observable

--- shared-editor-frontend/src/app/app.component.ts
+++ shared-editor-frontend/src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HubConnection } from '@microsoft/signalr';
+import { Observable, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Editor } from './features/editor';
+import { UserAction } from './features/user-action';
+import { UserActionConnection, EditorConnection } from './shared/connections';
+
+const mapToText = pipe(map((editor: Editor) => editor.text));
 
 @Component({
   selector: 'app-root',
@@ -6,7 +14,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  lastUserAction: Observable<UserAction> = this.fromConnectionToObservable<UserAction>(this.userActionConnection, 'user-action');
+  editorText: Observable<string> = this.fromConnectionToObservable<Editor>(this.editorConnection, 'editor').pipe(mapToText);
+
   constructor(
-    
+    @Inject(UserActionConnection) private userActionConnection: HubConnection,
+    @Inject(EditorConnection) private editorConnection: HubConnection
   ) { }
+
+  private fromConnectionToObservable<T>(connection: HubConnection, method: string): Observable<T> {
+    return new Observable(observer => {
+      connection.on(method, param => observer.next(param));
+      connection.start();
+    });
+  }
 }
--- shared-editor-frontend/src/app/features/user-action.ts
+++ shared-editor-frontend/src/app/features/user-action.ts
@@ -1,4 +1,4 @@
-import { InjectionToken } from '@angular/core';
+import { InjectionToken, Pipe, PipeTransform } from '@angular/core';
 import { Subject } from 'rxjs';
 
 export enum UserAction {
@@ -7,3 +7,18 @@ export enum UserAction {
 }
 
 export const UserActionSubject = new InjectionToken<Subject<UserAction>>('UserActionSubject');
+
+@Pipe({
+  name: 'userAction',
+  pure: true
+})
+export class UserActionPipe implements PipeTransform {
+  transform(value: UserAction | null): string {
+    switch (value) {
+      case UserAction.Joined: return 'User joined';
+      case UserAction.Left: return 'User left';
+      case null: return 'None';
+      default: throw new Error('Invalid UserAction');
+    }
+  }
+}
